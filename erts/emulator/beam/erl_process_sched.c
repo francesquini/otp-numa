8850f424655bcbd0348836f4618313ae836a4726
francesquini <emilio.francesquini@imag.fr>
2012-09-27 15:58:11 +0200

    Resolving declaration issues

--- erts/emulator/beam/erl_process_sched.c
+++ erts/emulator/beam/erl_process_sched.c
@@ -4,7 +4,7 @@
 
 static unsigned int (*PROC_SCHED_CURRENT_IP_STRATEGY)(Process*) = &proc_sched_ip_default;
 
-ERTS_GLB_INLINE void proc_sched_set_initial_placement_strategy (proc_sched_migration_strategy strategy) {
+void proc_sched_set_initial_placement_strategy (proc_sched_ip_strategy strategy) {
 	switch (strategy) {
 		case PROC_SCHED_IP_DEFAULT:
 			PROC_SCHED_CURRENT_IP_STRATEGY = &proc_sched_ip_default;
@@ -22,15 +22,16 @@ ERTS_GLB_INLINE void proc_sched_set_initial_placement_strategy (proc_sched_migra
 	}
 }
 
-ERTS_GLB_INLINE int proc_sched_initial_placement (Process* parent) {
+int proc_sched_initial_placement (Process* parent) {
 	return PROC_SCHED_CURRENT_IP_STRATEGY(parent);
 }
 
-ERTS_GLB_INLINE void proc_sched_check_balance (scheduling_data *sd) {
 
+void proc_sched_set_migration_strategy(proc_sched_migration_strategy strategy){
+}
+void proc_sched_check_balance (scheduling_data *sd) {
+}
+void proc_sched_immigrate (scheduling_data *sd) {
 }
 
-ERTS_GLB_INLINE void proc_sched_immigrate (scheduling_data *sd) {
-
-}
 
